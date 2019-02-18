21fc596e04474c1b14d90b4cb5c92faa851c8ccd
diop <fodesdiop@gmail.com>
2019-02-18 10:46:44 -0800

    add 404 status code

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func handlerFunc(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprint(w, "<h1>Welcome to the new classroom!</h1>")
 	} else if r.URL.Path == "/contact" {
 		fmt.Fprint(w, "To get in touch, please send an email to <a href=\"mailto:fode.diop@students.makeschool.com\">fode.diop@students.makeschool.com</a>")
+	} else {
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprint(w, "<h1>The page you're looking for has taken a vacation :( </h1><p>Please email us if you keep being sent to an invalid page</p>")
 	}
 }
 
